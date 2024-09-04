0dfd0230caf8faf2e598266465f58770d857a0ce
Lorenzo Hermoso <lmhm.yeah@gmail.com>
2024-09-04 12:38:26 +0200

    z-index fix

--- components/header.tsx
+++ components/header.tsx
@@ -6,23 +6,33 @@ import DiscordIcon from "./icons/DiscordIcon";
 import { FaDiscord } from "react-icons/fa";
 
 const Header = () => {
-    return (
-        <MotionTransition position="bottom" className="z-40 inline-block w-full">
-          <header className="w-full">
-            <div className="max-w-2xl mx-auto mt-[7%]">
-              <Link href="/">
-                  <img src="logo_grande.png" alt="Logo de la empresa" className="w-full"/>
-              </Link>
-            </div>
-            {/* <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10"> */}
-                <Link href='https://discord.gg/5htWutRn' className="flex items-center gap-2 bg-[#292b2f] w-fit p-2 rounded-xl text-white mx-auto mt-20">
-                  <FaDiscord className="w-6 h-6"/>
-                  <span>Join our Discord!</span>
-                </Link>
-{/*             </MotionTransition> */}
-          </header>
-        </MotionTransition>
-    );
-  };
+	return (
+		<MotionTransition
+			position='bottom'
+			className='z-30 inline-block w-full'
+		>
+			<header className='w-full'>
+				<div className='max-w-2xl mx-auto mt-[7%]'>
+					<Link href='/'>
+						<img
+							src='logo_grande.png'
+							alt='Logo de la empresa'
+							className='w-full'
+						/>
+					</Link>
+				</div>
+				{/* <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10"> */}
+				<Link
+					href='https://discord.gg/5htWutRn'
+					className='flex items-center gap-2 bg-[#292b2f] w-fit p-2 rounded-xl text-white mx-auto mt-20'
+				>
+					<FaDiscord className='w-6 h-6' />
+					<span>Join our Discord!</span>
+				</Link>
+				{/*             </MotionTransition> */}
+			</header>
+		</MotionTransition>
+	);
+};
 
 export default Header;
